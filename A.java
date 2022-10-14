efc9cdeafcad95339c53a2ffb9202176586d40f6
Vinayak989 <vinayakphatak000@gmail.com>
2022-10-14 20:40:37 +0530

    df

--- A.java
+++ A.java
@@ -8,3 +8,10 @@ System.out.println("I amfsgfd class A +dfdgi");
 	}
 
 }
+
+//git config --global user.name "Durgesh"
+//git config --global user.email "learncodewithdurgesh@gmail.com"
+
+//git remote add origin https://github.com/Vinayak989/github.git
+//git branch -M main
+//git push -u origin main
