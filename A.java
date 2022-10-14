1d35319678c1ed9947ea95abe09be67579334094
unknown <vinayakphatak000@gmail.com>
2022-10-14 20:13:56 +0530

    fvf

--- A.java
+++ A.java
@@ -4,7 +4,7 @@ public class A {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("I am class A +dfdgi");
+System.out.println("I amfsgfd class A +dfdgi");
 	}
 
 }
